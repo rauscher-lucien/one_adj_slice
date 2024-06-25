3c9587bd567c7f62eacfbe2e3be5061d0ea345a1
rauscher-lucien <rauscher.lucien@gmail.com>
2024-06-25 14:10:54 +0200

    added cuda inf

--- inference-cuda.py
+++ inference-cuda.py
@@ -1,22 +1,42 @@
 import os
 import sys
-sys.path.append(os.path.join(".."))
-
+import argparse
+import logging
+import glob
 import torch
 import numpy as np
 import tifffile
-import glob
 from torchvision import transforms
-import matplotlib.pyplot as plt
+
+sys.path.append(os.path.join(".."))
 
 from model import *
 from transforms import *
 from utils import *
 from dataset import *
 
+class StreamToLogger(object):
+    def __init__(self, logger, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
+
+    def flush(self):
+        pass
+
+def setup_logging(log_file='logging.log'):
+    logging.basicConfig(filename=log_file, filemode='a', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    logging.getLogger('').addHandler(console_handler)
+    sys.stdout = StreamToLogger(logging.getLogger('STDOUT'), logging.INFO)
+    sys.stderr = StreamToLogger(logging.getLogger('STDERR'), logging.ERROR)
 
 def load(checkpoints_dir, model, epoch=1, optimizer=None, device='cpu'):
-
     if optimizer is None:
         optimizer = torch.optim.Adam(model.parameters())  
 
@@ -34,37 +54,37 @@ def load(checkpoints_dir, model, epoch=1, optimizer=None, device='cpu'):
     return model, epoch
 
 def main():
+    setup_logging()
+    
+    parser = argparse.ArgumentParser(description='Process inference parameters.')
+    parser.add_argument('--project_dir', type=str, help='Path to the project directory', default=None)
+    parser.add_argument('--data_dir', type=str, help='Path to the data directory', default=None)
+    parser.add_argument('--device', type=str, default='cuda:0', help='Device to use for inference, e.g., "cuda:0" or "cpu"')
 
-    #********************************************************#
+    args = parser.parse_args()
 
-    project_dir = r"Z:\members\Rauscher\projects\one_adj_slice\big_data_small-no_nema-no_droso-test_1"
-    data_dir = r"C:\Users\rausc\Documents\EMBL\data\big_data_small\mouse_embryo"
+    if os.getenv('RUNNING_ON_SERVER') == 'true':
+        project_dir = args.project_dir
+        data_dir = args.data_dir
+    else:
+        project_dir = r"Z:\members\Rauscher\projects\one_adj_slice\big_data_small-no_nema-no_droso-test_1"
+        data_dir = r"Z:\members\Rauscher\data\big_data_small\platynereis"
+
+    device = torch.device(args.device if torch.cuda.is_available() else 'cpu')
+    
     project_name = os.path.basename(project_dir)
     inference_name = os.path.basename(data_dir)
-
-
-    #********************************************************#
-
+    
     results_dir = os.path.join(project_dir, 'results')
     checkpoints_dir = os.path.join(project_dir, 'checkpoints')
 
-    # Make a folder to store the inference
     inference_folder = os.path.join(results_dir, inference_name)
     os.makedirs(inference_folder, exist_ok=True)
     
-    ## Load image stack for inference
     filenames = glob.glob(os.path.join(data_dir, "*.tif")) + glob.glob(os.path.join(data_dir, "*.tiff"))
-    print("Following file will be denoised:  ", filenames)
+    print("Following files will be denoised:  ", filenames)
 
-
-
-    # check if GPU is accessible
-    if torch.cuda.is_available():
-        print("\nGPU will be used.")
-        device = torch.device("cuda:0")
-    else:
-        print("\nCPU will be used.")
-        device = torch.device("cpu")
+    print(f"Using device: {device}")
 
     mean, std = load_normalization_params(checkpoints_dir)
     
@@ -93,31 +113,24 @@ def main():
         num_workers=2
     )
 
-    
     model = NewUNet()
-    model, epoch = load(checkpoints_dir, model)
-
-    num_inf = len(inf_dataset)
-    num_batch = int((num_inf / batch_size) + ((num_inf % batch_size) != 0))
+    model, epoch = load(checkpoints_dir, model, device=device)
 
     print("starting inference")
-    output_images = []  # List to collect output images
+    output_images = []
 
     with torch.no_grad():
         model.eval()
-
         for batch, data in enumerate(inf_loader):
             input_img = data.to(device)
-
             output_img = model(input_img)
-            output_img_np = inv_inf_transform(output_img)  # Convert output tensors to numpy format for saving
+            output_img_np = inv_inf_transform(output_img)
 
             for img in output_img_np:
                 output_images.append(img)
 
             print('BATCH %04d/%04d' % (batch, len(inf_loader)))
     
-    # Stack and save output images
     output_stack = np.stack(output_images, axis=0)
     filename = f'output_stack-{project_name}-{inference_name}-epoch{epoch}.TIFF'
     tifffile.imwrite(os.path.join(inference_folder, filename), output_stack)
@@ -126,5 +139,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
